870645355e80fa86a87354305721c9842cb5304c
Liu (Dave) Liu <liudave.liu@gmail.com>
2023-01-23 14:50:34 -0600

    docker compose

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ base_dir='C:/Users/lliu10/OneDrive - Inside MD Anderson/Documents/Brain-Tumor-Pr
 
 #ds1 = pydicom.dcmread(base_dir+'/PGBM-001/04-02-1992-FH-HEADBrain Protocols-79896/11.000000-T1post-80644/1-13.dcm')
 
+#ds1 = pydicom.dcmread('/1-13.dcm')
 ds1 = pydicom.dcmread('/input/DICOM/1-13.dcm')
 ax = plt.hist(ds1.pixel_array.ravel(), bins = 256)
 
