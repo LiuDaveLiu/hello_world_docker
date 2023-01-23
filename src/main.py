4aebe525f3e2c47c80d7aa96795ef9615f407023
Liu (Dave) Liu <liudave.liu@gmail.com>
2023-01-23 14:19:57 -0600

    add config json

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import pydicom
 import matplotlib.pyplot as plt
+import os
 
 base_dir='C:/Users/lliu10/OneDrive - Inside MD Anderson/Documents/Brain-Tumor-Progression'
 
 #ds1 = pydicom.dcmread(base_dir+'/PGBM-001/04-02-1992-FH-HEADBrain Protocols-79896/11.000000-T1post-80644/1-13.dcm')
-ds1 = pydicom.dcmread('1-13.dcm')
+
+ds1 = pydicom.dcmread('/input/DICOM/1-13.dcm')
 ax = plt.hist(ds1.pixel_array.ravel(), bins = 256)
 
-plt.savefig('./first_plot1.png', transparent = True)
+plt.savefig('/output/first_plot1.png', transparent = True)
 
 print("Docker is magic!")
